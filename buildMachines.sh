7f4d5f04a264226e0e3ddcf1654ae80c2b2f17f0
Traceur70 <norman.adda@gmail.com>
2016-09-18 00:55:49 +0200

    Update buildMachines.sh

--- buildMachines.sh
+++ buildMachines.sh
@@ -2,3 +2,10 @@ cd /home
 sudo git clone https://github.com/Traceur70/dotnet-docker.git
 cd dotnet-docker/1.0.0-preview2/debian
 sudo docker build -t ul/dotnet .
+sudo docker run -dit -p 80:8080 --name dotnetCont ul/dotnet
+sudo docker attach dotnetCont
+cd home/
+git clone https://github.com/Traceur70/UL.CrossApp.git
+cd UL.CrossApp/UL.CrossApp.Web3/
+dotnet restore
+dotnet run &
