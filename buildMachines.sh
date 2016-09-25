1c0c7df93a206cab6d8036ce7de8101e4521bc49
Traceur70 <norman.adda@gmail.com>
2016-09-25 20:16:45 +0200

    Update buildMachines.sh

--- buildMachines.sh
+++ buildMachines.sh
@@ -4,12 +4,11 @@ ssh norman@containers03.cloudapp.net
 #Build & run a Debian server with .NET Core
 cd /home
 sudo git clone https://github.com/Traceur70/dotnet-docker.git
-cd dotnet-docker/1.0.0-preview2/debian
-sudo docker build -t ul/dotnet .
+sudo docker build -t ul/dotnet dotnet-docker/1.0.0-preview2/debian
 sudo docker run -dit -p 80:8080 --name sysWithDotnet ul/dotnet
 
 #Build & run an Ubuntu server without .NET Core
-sudo docker run -dit -p 80:8080 ubuntu:latest --name sysWithoutDotnet
+sudo docker run -dit -p 81:8080 --name sysWithoutDotnet ubuntu:latest 
 
 sudo docker attach dotnetCont
 cd home/
