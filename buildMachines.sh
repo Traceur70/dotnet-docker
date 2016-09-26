8d41f52f76d3a55efa40f9a110a9abbce5d81543
Traceur70 <norman.adda@gmail.com>
2016-09-26 14:17:28 +0200

    Update buildMachines.sh

--- buildMachines.sh
+++ buildMachines.sh
@@ -1,18 +1,22 @@
 #First connect to remote docker's machine 
 ssh norman@containers03.cloudapp.net
 
+sudo git clone https://github.com/Traceur70/dotnet-docker.git /home/dotnet-docker
+
 #Build & run a Debian server with .NET Core
-sudo git clone https://github.com/Traceur70/dotnet-docker.git /home
 sudo docker build -t ul/dotnet /home/dotnet-docker/1.0.0-preview2/debian
 sudo docker run -dit -p 80:8080 --name sysWithDotnet ul/dotnet
 
 #Build & run an Ubuntu server without .NET Core
-sudo docker run -dit -p 81:8080 --name sysWithoutDotnet ubuntu:latest 
+sudo docker build -t ul/nodotnet /home/dotnet-docker/1.0.0-preview2/ubuntuNoDotnet
+sudo docker run -dit -p 81:8080 --name sysWithoutDotnet ul/nodotnet
 
+#To run on sysWithDotnet
 sudo docker attach sysWithDotnet
-git clone https://github.com/Traceur70/UL.CrossApp.git /home
-dotnet restore /home/UL.CrossApp/UL.CrossApp.Web3/
-dotnet run /home/UL.CrossApp/UL.CrossApp.Web3/ &
-exit
+sudo git clone https://github.com/Traceur70/UL.CrossApp.git /home/UL.CrossApp
+cd /home/UL.CrossApp/src/UL.CrossApp.Web/
+sudo dotnet restore 
+sudo dotnet run &
+
 
 #End of script
