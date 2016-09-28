27f4d4ad557220426369f134979e52b965905adf
Traceur70 <norman.adda@gmail.com>
2016-09-28 14:35:20 +0200

    Update buildMachines.sh

--- buildMachines.sh
+++ buildMachines.sh
@@ -6,6 +6,7 @@ su
 curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
 chmod +x /usr/local/bin/docker-machine
 
+#Clone docker files repositories
 sudo git clone https://github.com/Traceur70/dotnet-docker.git /home/dotnet-docker
 
 #Build & run a Debian server with .NET Core
@@ -16,7 +17,7 @@ sudo docker run -dit -p 81:8080 --name sysWithDotnet ul/dotnet
 sudo docker build -t ul/nodotnet /home/dotnet-docker/1.0.0-preview2/ubuntuNoDotnet
 sudo docker run -dit -p 82:8080 --name sysWithoutDotnet ul/nodotnet
 
-#To run on sysWithDotnet
+#Get and run an ASP.NET Core app
 sudo docker attach sysWithDotnet
 sudo git clone https://github.com/Traceur70/UL.CrossApp.git /home/UL.CrossApp
 cd /home/UL.CrossApp/src/UL.CrossApp.Web/
